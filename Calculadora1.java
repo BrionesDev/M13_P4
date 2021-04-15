da62b1b2fc519975f8b31cc0d7618534284e68e5
ChristianCaballero29 <72620945+ChristianCaballero29@users.noreply.github.com>
2021-04-15 17:57:50 +0200

    Update Calculadora1.java

--- Calculadora1.java
+++ Calculadora1.java
@@ -21,7 +21,7 @@ public class Calculadora {
         System.out.println("Opció SORTIR seleccionada. Sortint del programa...");
     }
 
-    //Mostrar opcions de Menú
+    //Mostramos las opciones del menú
     public static void mostrarMenu() {
         System.out.println("___________________MENÚ___________________");
         System.out.println("1.-Suma . ");
@@ -35,7 +35,7 @@ public class Calculadora {
         System.out.println("9.- Sortir.");
     }
 
-    //Recorrer Menú
+    //Recorreremos el menú para seleccionar una de las opciones
     public static boolean llegirMenu() throws IOException {
         boolean sortir = false;
         int opcio = llegirInt("Selecciona una opció: ");
@@ -68,17 +68,17 @@ public class Calculadora {
                 sortir = true;
                 break;
             default:
-                System.out.println("Opció no vàlida, intena escollir una opció entre el 1 i el 2...");
+                System.out.println("Opció no vàlida, intena escollir una opció entre el 1 i el 9...");
                 break;
         }
         return sortir;
     }
-
+//Método que llamamos en el menú para multiplicar
     public static double Multiplicar(double n1, double n2) {
         System.out.println("El resultado es: " + n1 * n2);
         return n1 * n2;
     }
-
+//Método que llamamos en el menú para dividir
     public static double Dividir(double n1, double n2) {
         System.out.println("El resultado es: " + n1 / n2);
         if (n1 > n2) {
@@ -88,31 +88,32 @@ public class Calculadora {
             return n2 / n1;
         }
     }
-
+//Método que llamamos en el menú para clacular la raiz cuadrada
     public static double raizCuadrada(double num1) {
         double resultado = Math.sqrt(num1);
         System.out.println("El resultado es: " + resultado);
         return resultado;
     }
-
+//Método que llamamos en el menú para calcular la raiz cúbica
     public static double raizCubica(double num1) {
         double resultado = Math.sqrt(num1);
         System.out.println("El resultado es: " + resultado);
         return resultado;
     }
-
+//Método que llamamos en el menú para calcular el seno
     public static double seno(double num1) {
         double resultado = Math.sin(num1);
         System.out.println("El resultado es: " + resultado);
         return resultado;
     }
-
+//Método que llamamos en el menú para calcular el coseno
     public static double coseno(double num1) {
         double resultado = Math.sin(num1);
         System.out.println("El resultado es: " + resultado);
         return resultado;
     }
 
+    //lector de enteros con detención de errores
     public static int llegirInt(String xComent) {
         boolean intCorrecte;
         int x = 0;
@@ -125,6 +126,7 @@ public class Calculadora {
                 x = lector.nextInt();
                 lector.nextLine();
             } else {
+                //Mensajes que se mostrará en caso de no introducir un valor int en el menú
                 System.out.println("El valor introduït no és un número ENTER. Ha de ser-ho...");
                 System.out.print("Prova-ho de nou: ");
                 lector.nextLine();
